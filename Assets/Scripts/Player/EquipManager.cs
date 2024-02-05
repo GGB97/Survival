6a2bd639fb9eb82c745477da6fd64d2f5a483c36
GGB <rlqja1018@gmail.com>
2024-02-05 20:39:54 +0900

    feat - 공격/자원 채취 구현

---
+++ Assets/Scripts/Player/EquipManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EquipManager : MonoBehaviour
+{
+    public static EquipManager instance;
+
+    public Equip curEquip;
+    public Transform equipParent;
+
+    PlayerController controller;
+    PlayerConditions conditions;
+
+    private void Awake()
+    {
+        instance = this;
+        controller = GetComponent<PlayerController>();
+        conditions = GetComponent<PlayerConditions>();
+    }
+
+    public void OnAttackInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed && curEquip != null && controller.canLook)
+        {
+            curEquip.OnAttackInput();
+        }
+    }
+
+    public void EquipNew(ItemData item)
+    {
+        UnEquip();
+        curEquip = Instantiate(item.equipPrefab, equipParent).GetComponent<Equip>(); 
+    }
+
+    public void UnEquip()
+    {
+        if(curEquip != null)
+        {
+            Destroy(curEquip.gameObject);
+            curEquip = null;
+        }
+    }
+
+}
